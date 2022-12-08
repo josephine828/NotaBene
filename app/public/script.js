a9f099ca1d6212e0ffd8e157202bf89ddfddff66
josephine828 <114919028+josephine828@users.noreply.github.com>
2022-12-08 15:06:15 -0500

    Add task using advanced menu

--- app/public/script.js
+++ app/public/script.js
@@ -9,8 +9,16 @@ let taskContainer = document.getElementById("taskContainer");
 let quickAddTaskInput = document.getElementById("quickAddTaskInput");
 let quickAddTaskButton = document.getElementById("quickAddTaskButton");
 let noTaskMessage = document.getElementById("noTaskMessage");
+let addTaskMenu = document.getElementById("addTaskMenu");
+let editTaskMenu = document.getElementById("editTaskMenu");
+let listSelect = document.getElementById("listSelect");
+let categorySelect = document.getElementById("categorySelect");
+let addListButton = document.getElementById("addListButton");
+let addCategoryButton = document.getElementById("addCategoryButton");
 
 let tempTaskArray = [];
+let tempListArray = [];
+let tempCategoryArray = [];
 let taskCounter = 0;
 
 function init() {
@@ -27,9 +35,17 @@ function init() {
         multipleViewContainer.style.display = "block";
         singleViewContainer.style.display = "none";
     });
-    document.getElementById("closeSettingsButton").addEventListener("click", () => {
-        settingsPanel.style.display = "none";
-        darken.style.display = "none";
+    for (let closeButton of document.getElementsByClassName("closeButton")) {
+        closeButton.addEventListener("click", () => {
+            settingsPanel.style.display = "none";
+            addTaskMenu.style.display = "none";
+            editTaskMenu.style.display = "none";
+            darken.style.display = "none";
+        });
+    }
+    document.getElementById("addTaskButton").addEventListener("click", () => {
+        addTaskMenu.style.display = "block";
+        darken.style.display = "block";
     });
     quickAddTaskInput.addEventListener("keyup", (key) => {
         if (key.key === "Enter" && quickAddTaskInput.value !== "") {
@@ -41,26 +57,66 @@ function init() {
             quickAddTask(quickAddTaskInput.value);
         }
     });
+    listInput.addEventListener("keyup", (key) => {
+        if (key.key === "Enter" && addList.value !== "") {
+            addList(listInput.value);
+            loadListOptions();
+        }
+    });
+    addListButton.addEventListener("click", () => {
+        if (addList.value !== "") {
+            addList(listInput.value);
+            loadListOptions();
+        }
+    });
+    categoryInput.addEventListener("keyup", (key) => {
+        if (key.key === "Enter" && categoryInput.value !== "") {
+            addCategory(categoryInput.value);
+            loadCategoryOptions();
+        }
+    });
+    addCategoryButton.addEventListener("click", () => {
+        if (categoryInput.value !== "") {
+            addCategory(categoryInput.value);
+            loadCategoryOptions();
+        }
+    });
+    document.getElementById("addTaskMenuButton").addEventListener("click", () => {
+        addTask();
+    });
 }
 
 function quickAddTask(taskName) {
-    quickAddTaskInput.value = "";
-    noTaskMessage.style.display = "none";
+    if (tempTaskArray.includes(taskName)) {
+        alert("You already have a task with that name!")
+    }
+    else {
+        taskCounter++;
+        quickAddTaskInput.value = "";
+        noTaskMessage.style.display = "none";
+        let tempTask = {taskID: taskCounter, taskName: taskName, status: "incomplete", taskDescription: "", dueDate: null, list: "", categories: []};
+        tempTaskArray.push(tempTask);
+        addTaskToList(tempTask);
+    }
+}
 
-    tempTaskArray.push({taskID: taskCounter, taskName: taskName, status: "incomplete"});
-    console.log(tempTaskArray);
-
+function addTaskToList(taskObject) {
+    let taskName = taskObject.taskName;
     let taskDiv = document.createElement("div");
     taskDiv.textContent = taskName;
     taskDiv.classList.add("task");
-    taskDiv.setAttribute("id", taskCounter);
+    taskDiv.setAttribute("taskID", taskCounter);
+    taskDiv.addEventListener("click", () => {
+        editTaskMenu.style.display = "block";
+        darken.style.display = "block";
+    });
 
     let doneButton = document.createElement("button");
     doneButton.textContent = "\u2713";
     doneButton.classList.add("doneButton");
     doneButton.addEventListener("click", () => {
         doneButton.parentElement.classList.add("complete");
-        tempTaskArray[parseInt(doneButton.parentElement.getAttribute("id"))].status = "complete";
+        tempTaskArray[parseInt(doneButton.parentElement.getAttribute("taskID"))].status = "complete";
     });
 
     let deleteButton = document.createElement("button");
@@ -68,13 +124,150 @@ function quickAddTask(taskName) {
     deleteButton.classList.add("deleteButton");
     deleteButton.addEventListener("click", () => {
         deleteButton.parentElement.remove();
-        tempTaskArray = tempTaskArray.filter((task) => {return task.taskID !== parseInt(deleteButton.parentElement.getAttribute("id"))});
+        tempTaskArray = tempTaskArray.filter((task) => {return task.taskID !== parseInt(deleteButton.parentElement.getAttribute("taskID"))});
     });
-
-    taskCounter++;
+    
     taskDiv.append(doneButton);
     taskDiv.append(deleteButton);
     taskContainer.append(taskDiv);
+}
+
+function loadListOptions() {
+    if (tempListArray.length === 0) {
+        listSelect.textContent = "There are currently no lists to add the task to. Add a new list below!";
+    }
+    else {
+        listSelect.textContent = "";
+        for (let listOption of tempListArray) {
+            let div = document.createElement("div");
+            div.textContent = listOption;
+            div.addEventListener("click", () => {
+                if (document.getElementsByClassName("selectedList").length > 0 && !div.classList.contains("selectedList")) {
+                    alert("You can only select one list!");
+                }
+                else {
+                    if (div.classList.contains("selectedList")) {
+                        div.classList.remove("selectedList");
+                    }
+                    else {
+                        div.classList.add("selectedList");
+                    }
+                }
+            });
+            listSelect.append(div);
+        }
+    }
+}
+
+function loadCategoryOptions() {
+    if (tempCategoryArray.length === 0) {
+        categorySelect.textContent = "There are currently no categories to add the task to.";
+    }
+    else {
+        categorySelect.textContent = "";
+        for (let categoryOption of tempCategoryArray) {
+            let div = document.createElement("div");
+            div.textContent = categoryOption;
+            div.addEventListener("click", () => {
+                if (div.classList.contains("selectedCategory")) {
+                    div.classList.remove("selectedCategory");
+                }
+                else {
+                    div.classList.add("selectedCategory");
+                }
+            });
+            categorySelect.append(div);
+        }
+    }
+}
+
+function addList(listName) {
+    listInput.value = "";
+    if (!tempListArray.includes(listName)) {
+        tempListArray.push(listName);
+        updateListLegends();
+    }
+    else {
+        alert("You already have that list!");
+    }
+}
+
+function addCategory(categoryName) {
+    categoryInput.value = "";
+    if (!tempCategoryArray.includes(categoryName)) {
+        tempCategoryArray.push(categoryName);
+        updateCategoryLegend();
+    }
+    else {
+        alert("You already have that category!");
+    }
+}
+
+let taskNameInput = document.getElementById("taskNameInput");
+let taskDescriptionInput = document.getElementById("taskDescriptionInput");
+let dueDateInput = document.getElementById("dueDateInput");
+let listInput = document.getElementById("listInput");
+let categoryInput = document.getElementById("categoryInput");
+
+function addTask() {
+    if (taskNameInput.value === "") {
+        alert("You cannot leave the task name blank!");
+    }
+    else {
+        taskCounter++;
+        noTaskMessage.style.display = "none";
+        let selectedListElement = document.getElementsByClassName("selectedList")[0];
+        let selectedList = "";
+        if (selectedListElement !== undefined) {
+            selectedList = selectedListElement.textContent;
+        }
+        let selectedCategoryElements = document.getElementsByClassName("selectedCategory");
+        let selectedCategories = [];
+        for (let selectedCategoryElement of selectedCategoryElements) {
+            selectedCategories.push(selectedCategoryElement.textContent);
+        }
+        let tempTask = {taskID: taskCounter, taskName: taskNameInput.value, status: "incomplete", taskDescription: taskDescriptionInput.value, dueDate: dueDateInput.value, list: selectedList, categories: selectedCategories};
+        tempTaskArray.push(tempTask);
+        addTaskToList(tempTask);
+        taskNameInput.value = "";
+        taskDescriptionInput.value = "";
+        dueDateInput.value = "";
+        listInput.value = "";
+        categoryInput.value = "";
+        addTaskMenu.style.display = "none";
+        darken.style.display = "none";
+    }
+}
+
+let listsLegend = document.getElementById("listsLegend");
+let categoriesLegend = document.getElementById("categoriesLegend");
+
+function updateListLegends() {
+    if (tempListArray.length > 0) {
+        listsLegend.textContent = "";
+        for (let tempList of tempListArray) {
+            let div = document.createElement("div");
+            div.textContent = tempList;
+            listsLegend.append(div);
+        }
+    }
+    else {
+        listsLegend.textContent = "No Lists";
+    }
+}
+
+function updateCategoryLegend() {
+    categoriesLegend.textContent = "";
+    if (tempCategoryArray.length > 0) {
+        for (let tempCategory of tempCategoryArray) {
+            let div = document.createElement("div");
+            div.textContent = tempCategory;
+            categoriesLegend.append(div);
+        }
+    }
+    else {
+        categoriesLegend.textContent = "No Categories";
+    }
 }
 
 function settingsPanelAction() {
