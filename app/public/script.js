068f27915c50c00e230ff7bdda1f2d70de358a7b
josephine828 <114919028+josephine828@users.noreply.github.com>
2022-12-08 10:36:26 -0500

    Able to switch between views

--- app/public/script.js
+++ app/public/script.js
@@ -1,13 +1,25 @@
 let settingsTab = document.getElementById("settingsTab");
 let settingsPanel = document.getElementById("settingsPanel");
 let darken = document.getElementById("darken");
-
-$(document).ready(function() {
-    $('.js-example-basic-single').select2();
-});
+let singleView = document.getElementById("singleView");
+let multipleView = document.getElementById("multipleView");
+let singleViewContainer = document.getElementById("singleViewContainer");
+let multipleViewContainer = document.getElementById("multipleViewContainer");
 
 function init() {
     settingsTab.addEventListener("click", settingsPanelAction);
+    singleView.addEventListener("click", () => {
+        multipleView.className = "";
+        singleView.className = "selectedView";
+        singleViewContainer.style.display = "block";
+        multipleViewContainer.style.display = "none";
+    });
+    multipleView.addEventListener("click", () => {
+        singleView.className = "";
+        multipleView.className = "selectedView";
+        multipleViewContainer.style.display = "block";
+        singleViewContainer.style.display = "none";
+    });
 }
 
 function settingsPanelAction() {
